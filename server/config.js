6b7ec7f2c0fe5ef4d25ffe8abb9a11b7f5b09362
yarikxan <65306929+yarikxan@users.noreply.github.com>
2024-10-10 16:21:22 +0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
     database: {
-        url: 'mongodb://admin:%5Bfrthbgbljhs2@38.114.102.237:27017/Cards?authSource=admin',
+        url: 'ult',
     },
     server: {
         PORT: 3000,
@@ -8,10 +8,10 @@ const config = {
     },
     mailer: {
         host: 'gmail',
-        user: 'yarikxan2@gmail.com',
-        pass: 'qxux scya apuk annz',
+        user: 'user',
+        pass: 'pass',
     },
-    secret: '40eca18c4bd340dc028165a8913e26a1416d5f272928ce96936788104321825753328c6e301d774e94220bf3a1f42fc5a6661a58717228f8214351543af4a261',
+    secret: 'secret',
     other: {
         pagination: {
             pagesize: 12 //amount of cards on single page
