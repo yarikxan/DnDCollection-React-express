c21d764888d35ee180968b386e540a0eaf8c98cc
yarikxan <yarikxan2@gmail.com>
2024-10-10 16:25:49 +0300

    ignoretest

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
     database: {
-        url: 'ult',
+        url: 'mongodb://admin:%5Bfrthbgbljhs2@38.114.102.237:27017/Cards?authSource=admin',
     },
     server: {
         PORT: 3000,
@@ -8,10 +8,10 @@ const config = {
     },
     mailer: {
         host: 'gmail',
-        user: 'user',
-        pass: 'pass',
+        user: 'yarikxan2@gmail.com',
+        pass: 'qxux scya apuk annz',
     },
-    secret: 'secret',
+    secret: '40eca18c4bd340dc028165a8913e26a1416d5f272928ce96936788104321825753328c6e301d774e94220bf3a1f42fc5a6661a58717228f8214351543af4a261',
     other: {
         pagination: {
             pagesize: 12 //amount of cards on single page
