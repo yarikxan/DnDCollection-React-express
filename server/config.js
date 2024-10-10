c34bc097c31495b9ccb1b2978cf658abbdcb1d89
yarikxan <yarikxan2@gmail.com>
2024-10-10 16:29:33 +0300

    stillignoretest

--- server/config.js
+++ server/config.js
@@ -9,7 +9,7 @@ const config = {
     mailer: {
         host: 'gmail',
         user: 'yarikxan2@gmail.com',
-        pass: 'qxux scya apuk annz',
+        pass: '',
     },
     secret: '40eca18c4bd340dc028165a8913e26a1416d5f272928ce96936788104321825753328c6e301d774e94220bf3a1f42fc5a6661a58717228f8214351543af4a261',
     other: {
